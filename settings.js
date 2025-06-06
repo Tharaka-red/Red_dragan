223733a3ae97f0c52a7e7ca465d7b3e77350e4c2
Tharaka-red <t2008l1222@gmail.com>
2025-06-06 16:17:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Red_dragan',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Red_dragan",
+  botOwner: 'Tharaka lakshan', // Your name
+  ownerNumber: '94 78 690 8427', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
